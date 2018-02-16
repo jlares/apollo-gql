c108215906599487c2f4eb1b6bc0e07a431731e5
Jo Lares <jo_lares@ultimatesoftware.com>
2018-02-16 08:02:22 -0500

    chore: Install Apollo & GQL

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+
+
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 
 import { AppComponent } from './app.component';
@@ -10,9 +16,25 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    
+    ApolloModule,
+    HttpLinkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    apollo: Apollo,
+    httpLink: HttpLink
+  ) {
+    apollo.create({
+      // By default, this client will send queries to the `/graphql` endpoint on
+      // the same host.
+      link: httpLink.create({ uri: 'https://api.example.com/graphql' }),
+      cache: new InMemoryCache()
+    });
+  }
+ }
